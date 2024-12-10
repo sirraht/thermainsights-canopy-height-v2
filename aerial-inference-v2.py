3a39433e4ed6c962cdb60a9e1e8b45028223be89
T Harris <atharris3@gmail.com>
2024-12-10 12:04:27 -0700

    fixing inference tile sizing problem

--- aerial-inference-v2.py
+++ aerial-inference-v2.py
@@ -61,7 +61,6 @@ class SSLModule(pl.LightningModule):
             state_dict = ckpt['state_dict']
             self.chm_module_.load_state_dict(state_dict)
         
-        # Scale output by 10 to get canopy height
         self.chm_module = lambda x: 10 * self.chm_module_(x)
 
     def forward(self, x):
@@ -69,7 +68,7 @@ class SSLModule(pl.LightningModule):
         return x
 
 class AerialImageDataset(torch.utils.data.Dataset):
-    def __init__(self, image_dir, transform=None, size=256):
+    def __init__(self, image_dir, transform=None, patch_size=256, stride=256):
         self.image_dir = Path(image_dir)
         self.image_paths = (
             list(self.image_dir.glob('*.jpg')) +
@@ -78,7 +77,8 @@ class AerialImageDataset(torch.utils.data.Dataset):
             list(self.image_dir.glob('*.jpeg'))
         )
         self.transform = transform
-        self.size = size
+        self.patch_size = patch_size
+        self.stride = stride
         
     def __len__(self):
         return len(self.image_paths)
@@ -86,93 +86,91 @@ class AerialImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         img_path = self.image_paths[idx]
         img = Image.open(img_path).convert('RGB')
-        img = img.resize((self.size, self.size), Image.BILINEAR)
-        if self.transform:
-            img = self.transform(img)
-        else:
-            img = T.ToTensor()(img)
-        return {'img': img, 'img_path': str(img_path)}
+        img_width, img_height = img.size
+
+        patches = []
+        for y in range(0, img_height, self.stride):
+            for x in range(0, img_width, self.stride):
+                crop_box = (x, y, x + self.patch_size, y + self.patch_size)
+                patch = img.crop(crop_box)
+                
+                if self.transform:
+                    patch = self.transform(patch)
+                else:
+                    patch = T.ToTensor()(patch)
+                
+                patches.append({'img': patch, 'img_path': str(img_path), 'x_idx': x, 'y_idx': y, 'img_width': img_width, 'img_height': img_height})
+                
+        return patches
 
 def evaluate(model, norm, model_norm, preprocessed_dir, bs=32, trained_rgb=False, normtype=2, device='cuda:0', display=False, image_dir='./data/images/'):
     print("normtype", normtype)    
-    ds = AerialImageDataset(image_dir=image_dir, size=256)
-    dataloader = torch.utils.data.DataLoader(ds, batch_size=bs, shuffle=False, num_workers=4)
+    ds = AerialImageDataset(image_dir=image_dir, patch_size=256, stride=256)
+    dataloader = torch.utils.data.DataLoader(ds, batch_size=1, shuffle=False, num_workers=4)
         
-    # Save predictions under preprocessed_dir/predicted_canopy_heights
     preprocessed_path = Path(preprocessed_dir)
-    output_images_dir = preprocessed_path / 'predicted_canopy_heights'
+    output_images_dir = preprocessed_path / 'reassembled_canopy_heights'
     output_images_dir.mkdir(parents=True, exist_ok=True)
 
     model.eval()
     with torch.no_grad():
         for batch in tqdm(dataloader):
-            img = batch['img'].to(device)
-            img_paths = batch['img_path']
+            patches = batch[0]
+            img_path = patches[0]['img_path']
+            img_width = patches[0]['img_width']
+            img_height = patches[0]['img_height']
             
-            # Apply normalization
-            img_norm = norm(img)
+            reassembled_image = np.zeros((img_height, img_width), dtype=np.float32)
             
-            # Model prediction
-            pred = model(img_norm)
-            pred = pred.cpu().detach().relu()
-            
-            # Save predictions as TIFF (float32)
-            for i in range(pred.size(0)):
-                pred_img = pred[i][0].numpy()
-                pred_image = Image.fromarray(pred_img.astype('float32'), mode='F')
-                # The tile name is something like tile_{y_index}_{x_index}_context
-                # We append "_pred.tif"
-                tif_output_path = output_images_dir / (Path(img_paths[i]).stem + '_pred.tif')
-                pred_image.save(tif_output_path)
+            for patch in patches:
+                img = patch['img'].unsqueeze(0).to(device)  # Add batch dimension
+                x_idx, y_idx = patch['x_idx'], patch['y_idx']
+                
+                img_norm = norm(img)
+                pred = model(img_norm)
+                pred = pred.cpu().detach().relu()
+                
+                pred_img = pred[0][0].numpy()
+                h, w = pred_img.shape
+                reassembled_image[y_idx:y_idx+h, x_idx:x_idx+w] = pred_img
 
-    print(f"Prediction tiles saved under {output_images_dir}")
+            # Save the reassembled image as a single output file
+            reassembled_image_pil = Image.fromarray(reassembled_image.astype('float32'), mode='F')
+            output_path = output_images_dir / (Path(img_path).stem + '_reassembled.tif')
+            reassembled_image_pil.save(output_path)
+
+    print(f"Reassembled images saved under {output_images_dir}")
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Run canopy height inference on aerial images.')
     parser.add_argument('--checkpoint', type=str, help='CHM pred checkpoint file', default='saved_checkpoints/compressed_SSLlarge.pth')
-    parser.add_argument('--preprocessed_dir', type=str, required=True, help='Directory created during preprocessing for a specific NAIP image. Predictions will be placed here.')
+    parser.add_argument('--preprocessed_dir', type=str, required=True, help='Directory for preprocessed predictions.')
     parser.add_argument('--trained_rgb', action='store_true', help='Set if model was finetuned on aerial data')
-    parser.add_argument('--normnet', type=str, help='Path to a normalization network', default='saved_checkpoints/aerial_normalization_quantiles_predictor.ckpt')
+    parser.add_argument('--normnet', type=str, help='Path to normalization network', default='saved_checkpoints/aerial_normalization_quantiles_predictor.ckpt')
     parser.add_argument('--normtype', type=int, help='0: no norm; 1: old norm; 2: new norm', default=2) 
-    parser.add_argument('--display', action='store_true', help='If set, save additional PNG visualizations (not required).')
+    parser.add_argument('--display', action='store_true', help='If set, save additional PNG visualizations.')
     parser.add_argument('--image_dir', type=str, help='Directory containing input aerial tiles', default='./data/images/')
     args = parser.parse_args()
     return args
 
 def main():
     args = parse_args()
-    if 'compressed' in args.checkpoint:
-        device = 'cpu'
-    else:
-        device = 'cuda:0'
+    device = 'cuda:0' if 'compressed' not in args.checkpoint else 'cpu'
     
-    # Load normalization network
-    norm_path = args.normnet 
-    ckpt = torch.load(norm_path, map_location='cpu')
+    ckpt = torch.load(args.normnet, map_location='cpu')
     state_dict = ckpt['state_dict']
-    for k in list(state_dict.keys()):
-        if 'backbone.' in k:
-            new_k = k.replace('backbone.', '')
-            state_dict[new_k] = state_dict.pop(k)
-    
     model_norm = RNet(n_classes=6)
-    model_norm = model_norm.eval()
     model_norm.load_state_dict(state_dict)
         
-    # Load SSL model
     model = SSLModule(ssl_path=args.checkpoint)
     model.to(device)
     model = model.eval()
     
-    # Image normalization
     norm = T.Normalize((0.420, 0.411, 0.296), (0.213, 0.156, 0.143))
     norm = norm.to(device)
     
-    # Evaluate 
     evaluate(
-        model,
-        norm,
-        model_norm,
+        model, norm, model_norm,
         preprocessed_dir=args.preprocessed_dir,
         bs=16,
         trained_rgb=args.trained_rgb,
